c02b995c4275a6669f4d21feb1bd9d042e069001
sumit <sk7627577@gmail.com>
2025-06-18 20:59:08 +0530

    fix: update heading text for outside Canada form page

--- src/app/outside-canada/page.tsx
+++ src/app/outside-canada/page.tsx
@@ -3,7 +3,7 @@ import Form from "./components/Form"
 const page = () => {
     return (
         <div>
-            <h2 className="text-center my-8 text-white font-medium text-xl md:text-2xl bg-primary max-w-3xl py-8 mx-auto">Please Fill this form if you or your sponsor is living inside Canada. </h2>
+            <h2 className="text-center my-8 text-white font-medium text-xl md:text-2xl bg-primary max-w-3xl py-8 mx-auto">Please Fill this form if you or your sponsor is living outside Canada. </h2>
            <Form />
         </div>
     );
