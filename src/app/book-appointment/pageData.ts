1a3315879804aae7d85d82a06737c41c903af77f
sumit kumar <sumit@eazotel.com>
2025-12-13 17:00:38 +0530

    arnge data

--- src/app/book-appointment/pageData.ts
+++ src/app/book-appointment/pageData.ts
@@ -1,19 +1,5 @@
-
 export const pageData = {
   bookAppointment: [
-    {
-      src: "/Rhea.jpg",
-      links: [
-        {
-          label: "30 Minutes Appointment",
-          href: "https://calendly.com/tncconsult/30-minute-consultation-with-tnc-immigration",
-        },
-        {
-          label: "60 Minutes Appointment",
-          href: "https://calendly.com/tncconsult/60-minute-consultation-with-tnc-immigration",
-        },
-      ],
-    },
     {
       src: "/Prabhleen.jpg",
       links: [
@@ -24,6 +10,19 @@ export const pageData = {
         {
           label: "60 Minutes Appointment",
           href: "https://calendly.com/tncbooking/consultation60",
+        },
+      ],
+    },
+    {
+      src: "/Rhea.jpg",
+      links: [
+        {
+          label: "30 Minutes Appointment",
+          href: "https://calendly.com/tncconsult/30-minute-consultation-with-tnc-immigration",
+        },
+        {
+          label: "60 Minutes Appointment",
+          href: "https://calendly.com/tncconsult/60-minute-consultation-with-tnc-immigration",
         },
       ],
     },
