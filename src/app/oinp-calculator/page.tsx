c88ca2aef799e9271327262dc35571c7fe095111
Personal <sk7627577@gmail.com>
2026-07-28 23:02:06 +0530

    up

--- src/app/oinp-calculator/page.tsx
+++ src/app/oinp-calculator/page.tsx
@@ -5,7 +5,7 @@ export default function OinpCalculatorPage() {
         id="oinp-calculator"
         title="TNC Main Inside"
         src="/oinp-calculator.html"
-        className="md:h-[900px] h-[1000px]"
+        className="md:h-[1750px] h-[1000px]"
         style={{
           minWidth: "100%",
           maxWidth: "100%",
